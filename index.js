2b63fb08b896f6ee42b99908bdadf159c694d2a4
Sergey Smirnov <sergous@gmail.com>
2018-02-27 16:08:39 +0200

    Mark wrong ru.

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function onOpen() {
    */
   
   function addTranslations() {
-    
     var spreadsheet = SpreadsheetApp.getActive();
     
     
@@ -32,7 +31,7 @@ function onOpen() {
     
     var updatesTable = spreadsheet.getSheetByName(updateTableName);
     
-    var updatesDataRange = TABLE_RANGE;
+    // var updatesDataRange = TABLE_RANGE;
     // var updatesDataRange = askData('Укажите диапазон', 'Укажите диапазон данных в таблице новых переводов' + ' (например, "A1:J1958"):');
     
     
@@ -57,75 +56,112 @@ function onOpen() {
     var baseRuIdx = findColIndexByColName(baseTable, 'ru');
     if (!baseRuIdx) throw new Error('addTranslations: baseRuIdx is not set');
     
+    var updatesRuIdx = findColIndexByColName(baseTable, 'ru');
+    if (!updatesRuIdx) throw new Error('addTranslations: updatesRuIdx is not set');
+    
     updatesTable.activate();
     
     for(var idx = startRowIdx; idx <= endRowIdx; idx++) {
-      var replacedIdx = replaceRow(idx, baseRuIdx);
+      var replacedIdx = replaceRow(baseTable, updatesTable, idx, baseRuIdx);
       
       if (!replacedIdx) {
-        setTableCellBg(UPDATES_TABLE_NAME, idx, 1);
         statNotFoundLineIds.push(idx);
-        break;
+        continue;
       }
   
       statFoundLinesIdx.push(replacedIdx + 1); 
     }
     
-    Browser.msgBox('Результы обновления переводов', 
-                   'Обновлены строки (' + statFoundLinesIdx.length + '): ' + statFoundLinesIdx.join(',') + ' | ' +
-                   'Не найдены ключи: (' + statNotFoundLineIds.length + '): ' + statNotFoundLineIds.join(',')
-                   , Browser.Buttons.OK);
+    var results = 'Обновлены строки (' + statFoundLinesIdx.length + '): ' + statFoundLinesIdx.join(',') + ' | ' +
+      'Не найдены ключи (' + statNotFoundLineIds.length + '): ' + statNotFoundLineIds.join(',');
+    var title = 'Результы обновления переводов';
+    
+    Logger.log(title + ': ' + results);
+    
+    Browser.msgBox(title, results, Browser.Buttons.OK);
+  }
   
-    function setTableCellBg(tableName, rowIdx, colIdx, color) {
-      var context = 'setTableCellBg: '; 
-      if (!tableName) throw new Error(context + 'tableName is not set');
-      if (!rowIdx) throw new Error(context + 'rowIdx is not set');
-      if (!colIdx) throw new Error(context + 'colIdx is not set');
-      if (!color) color = "red";
-      
-      var spreadsheet = SpreadsheetApp.getActive();
-      var table = spreadsheet.getSheetByName(tableName);
-      var range = table.getRange(rowIdx, colIdx, 1, 1);
-      range.setBackground(color);
-      Logger.log('Marked as error: row ' + rowIdx + 1 + ' column ' + colIdx);
+  function replaceRow(baseTable, updatesTable, rowIdx, baseRuIdx) {
+    var context = 'replaceRow: '; 
+    if (!baseTable) throw new Error(context + 'baseTable is not set');
+    if (!updatesTable) throw new Error(context + 'updatesTable is not set');
+    if (!rowIdx) throw new Error(context + 'rowIdx is not set');
+    if (!baseRuIdx) throw new Error(context + 'baseRuIdx is not set');
+    
+    // Get translate id 
+    var translateId = findTranslateId(updatesTable, TABLE_RANGE, rowIdx);
+    if (!translateId) {
+      setTableCellBg(UPDATES_TABLE_NAME, rowIdx, 1);
+      return;
     }
     
-    function replaceRow(rowId, baseRuIdx) {
-      var context = 'replaceRow: '; 
-      if (!rowId) throw new Error(context + 'rowId is not set');
+    Logger.log('translateId ' + translateId);
+    
+    // var updateColName = 'de';
+    // var updateColName = askData('Укажите язык', 'Укажите язык' + ' (например, "de"):');
+    
+    // var updatesColIdx = findColIndexByColName(updatesTable, updateColName);
+    // Logger.log('RU ' + updatesRuId + ' DE ' + updatesDeId);
+    
+    // Found updates table row values for translateId
+    // var foundUpdatesRow = getRowByTranslateId(updatesTable, TABLE_RANGE, translateId).data;
+    // Logger.log('foundUpdatesRow ' + foundUpdatesRow);
+    
+    var updatesRow = getRowByTranslateId(updatesTable, TABLE_RANGE, translateId);
+    if (!updatesRow) throw new Error(context + 'updatesRow is not found'); 
+    
+    var foundUpdatesTranslations = updatesRow.translations;
+    Logger.log('foundUpdatesTranslations ' + foundUpdatesTranslations);
+    
+    // var updatedValue = foundUpdatesRow[updatesColIdx];
+    // Logger.log('updateColName ' + updateColName + ' updatedValue ' + updatedValue);
+    
+    // Found base table row with translateId
+    var baseRow = getRowByTranslateId(baseTable, TABLE_RANGE, translateId);
+    if (!baseRow) {
+      setTableCellBg(UPDATES_TABLE_NAME, rowIdx, 1);
+      return;
+    } else {
+      var baseRowIdx = baseRow.index;
+      var foundBaseTranslations = baseRow.translations;
       
-      // Get translate id 
-      var translateId = findTranslateId(updatesTable, updatesDataRange, rowId);
-      Logger.log('translateId ' + translateId);
+      var updatesRuIdx = findColIndexByColName(updatesTable, 'ru');
       
-      // var updateColName = 'de';
-      // var updateColName = askData('Укажите язык', 'Укажите язык' + ' (например, "de"):');
+      Logger.log('foundBaseTranslations ' + foundBaseTranslations);
       
-      // var updatesColIdx = findColIndexByColName(updatesTable, updateColName);
-      // Logger.log('RU ' + updatesRuId + ' DE ' + updatesDeId);
-      
-      // Found updates table row values for translateId
-      // var foundUpdatesRow = getRowByTranslateId(updatesTable, updatesDataRange, translateId).data;
-      // Logger.log('foundUpdatesRow ' + foundUpdatesRow);
-      
-      var foundUpdatesTranslations = getRowByTranslateId(updatesTable, updatesDataRange, translateId).translations;
-      Logger.log('foundUpdatesTranslations ' + foundUpdatesTranslations);
-      
-      // var updatedValue = foundUpdatesRow[updatesColIdx];
-      // Logger.log('updateColName ' + updateColName + ' updatedValue ' + updatedValue);
-      
-      // Found base table row with translateId
-      var baseRow = getRowByTranslateId(baseTable, TABLE_RANGE, translateId);
-      if (!baseRow) {
+      if (!isSameValue(foundBaseTranslations, foundUpdatesTranslations)) {
+        setTableCellBg(UPDATES_TABLE_NAME, rowIdx, updatesRuIdx + 1);
         return;
-      } else {
-        var baseRowIdx = baseRow.index;
-        
-        replaceTableRow(baseTable, baseRowIdx + 1, baseRuIdx + 1, foundUpdatesTranslations);
-        return baseRowIdx;
-      } 
-    }
+      }
+      
+      replaceTableRow(baseTable, baseRowIdx + 1, baseRuIdx + 1, foundUpdatesTranslations);
+      return baseRowIdx;
+    } 
   }
+  
+  function setTableCellBg(tableName, rowIdx, colIdx, color) {
+    var context = 'setTableCellBg: '; 
+    if (!tableName) throw new Error(context + 'tableName is not set');
+    if (!rowIdx) throw new Error(context + 'rowIdx is not set');
+    if (!colIdx) throw new Error(context + 'colIdx is not set');
+    if (!color) color = "red";
+    
+    var spreadsheet = SpreadsheetApp.getActive();
+    var table = spreadsheet.getSheetByName(tableName);
+    var range = table.getRange(rowIdx, colIdx, 1, 1);
+    range.setBackground(color);
+    Logger.log('WARNING! Marked as error: row ' + rowIdx + ' column ' + colIdx);
+  }
+  
+  function isSameValue(firstArray, secondArray, index) {
+    var context = 'isSameValue: '; 
+    if (!firstArray) throw new Error(context + 'firstArray is not set');
+    if (!secondArray) throw new Error(context + 'secondArray is not set');
+    if (!index) index = 0;
+    
+    return firstArray[index] === secondArray[index];
+  }
+  
   
   function translateIdResults(table, translateId) {
     var context = 'translateIdResults: '; 
@@ -189,17 +225,17 @@ function onOpen() {
     return data;
   }
   
-  function findTranslateId(table, dataRange, rowId) {
+  function findTranslateId(table, dataRange, rowIdx) {
     var context = 'findTranslateId: '; 
     if (!table) throw new Error(context + 'table is not set');
     if (!dataRange) throw new Error(context + 'dataRange is not set');
-    if (!rowId) throw new Error(context + 'rowId is not set');
+    if (!rowIdx) throw new Error(context + 'rowIdx is not set');
     
     var row = table.getRange(dataRange);
     var rowValues = row.getValues();
-    var foundId = rowValues[rowId - 1][findColIndexByColName(table, TRANSLATE_ID_NAME)];
-    // var en = rowValues[rowId - 1][findColIndexByColName(table, 'en')];
-    // var de = rowValues[rowId - 1][findColIndexByColName(table, 'de')];
+    var foundId = rowValues[rowIdx - 1][findColIndexByColName(table, TRANSLATE_ID_NAME)];
+    // var en = rowValues[rowIdx - 1][findColIndexByColName(table, 'en')];
+    // var de = rowValues[rowIdx - 1][findColIndexByColName(table, 'de')];
     return foundId;
   }
   
